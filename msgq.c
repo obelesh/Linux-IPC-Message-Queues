2b82a1de9de1fce4be223ccceb8cd7622d466e23
obelesh <obelesh@localhost.localdomain>
2025-04-24 04:38:34 -0400

    ex04

--- msgq.c
+++ msgq.c
@@ -1,28 +1,4 @@
-# Linux-IPC-Message-Queues
-Linux IPC-Message Queues
-
-# AIM:
-To write a C program that receives a message from message queue and display them
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux message queues API 
-
-### Step 3:
-
-Execute the C Program for the desired output. 
-
-# PROGRAM:
-
-## C program that receives a message from message queue and display them
-
-```c
+// msqueue.c - Combined Writer/Reader for System V Message Queue
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -56,7 +32,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(argv[1], "writer") == 0) {
-        message.mesg_type = 1;
+        message.mesg_type = 1
         printf("Enter Message: ");
         fgets(message.mesg_text, sizeof(message.mesg_text), stdin);
         message.mesg_text[strcspn(message.mesg_text, "\n")] = 0; // remove newline
@@ -80,13 +56,4 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-```
 
-## OUTPUT
-
-![ex04-writer](./img/img1.png)
-
-![ex04-reciver](./img/img2.png)
-
-# RESULT:
-The programs are executed successfully.
